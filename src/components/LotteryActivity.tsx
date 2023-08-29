44c6dd23095879587861a119496dde94e754eefa
HarshaVardhanJalli <53273666+HarshaVardhanJalli@users.noreply.github.com>
2023-08-29 13:37:57 +0530

    Styling changes

--- src/components/LotteryActivity.tsx
+++ src/components/LotteryActivity.tsx
@@ -7,21 +7,26 @@ const LotteryActivity = (props:any) => {
   return (
     <div className="lotteryActivity">
         {unPickedNumbers.length > 0 ? <Button type="primary" className='generateBtn' onClick={() => {generateRandomNumber()}}>{pickedNumbers.length === 0 ? 'Start Game' : (unPickedNumbers.length === 0 ? 'Game Over!' : 'Generate Number')}</Button> : 
-        <Button type="primary" className='generateBtn' onClick={() => {resetGame()}}>Reset Game</Button>}
+        <Button type="primary" className='generateBtn' onClick={() => {resetGame()}}>Restart Game</Button>}
         <Card className="currentNumber" style={{ width: 300 }}>
             <p>{currentNumber}</p>
         </Card>
         <Card style={{ width: 300 }}>
-            <Card className="unPickedCount"  style={{ width: 150 }}>
-                Left {unPickedNumbers.length}
+            <Card   style={{ width: 150 }}>
+                <div>
+                    Left <span className="unPickedCount">{unPickedNumbers.length}</span>
+                </div>
             </Card>
-            <Card className="pickedCount" style={{ width: 150 }}>
-                Done {pickedNumbers.length}
+            <Card  style={{ width: 150 }}>
+                <div>
+                    Done <span className="pickedCount">{pickedNumbers.length}</span>
+                </div>
             </Card>
         </Card>
-        <Card className="lastFive" style={{ width: 300 }}>
+        <Card className="lastFive" style={{ width: 300, height: 50 }}>
             <p>{pickedNumbers.slice(-5).join(" , ")}</p>
         </Card>
+        <Button type="primary" className='resetBtn' onClick={() => {resetGame()}} danger>Reset Game</Button>
     </div>
   );
 }
